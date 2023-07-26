9c4df15c840e80790e8e0fd970ebcc111ee603ca
Antoine Struelens <antoine.struelens@gmail.com>
2023-07-26 13:52:55 +0200

    clean up unused code

--- index.mjs
+++ index.mjs
@@ -3,24 +3,21 @@ const app = express();
 const PORT = 9000;
 import file from "./data.json" assert { type: "json" };
 
-
-
-// const postman = require('postman');
 app.set('view engine','ejs')
-
 app.use(express.json());
 app.use(express.static("public"));
-// app.use(express.urlencoded({ extended: true }))
 
+//GET request response giving the entire JSON array for all recipes
 app.get("/", (req, res) => {
   res.send(file);
 });
 
+//GET request response giving available endpoints
 app.get("/info", (req, res) => {
-  // res.send("coucou")
   res.render('info', {data: file});
 });
 
+//GET request responses giving the JSON object for each recipe
 for (let i = 1; i <= file.length; i++){
 app.get(`/recipe${i}`, (req,res)=> {
     res.send(file[i-1])
