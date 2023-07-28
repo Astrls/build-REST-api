c921003b95687c9071b18993b94fc61c012c351a
Antoine Struelens <antoine.struelens@gmail.com>
2023-07-28 12:26:05 +0200

    authorization via API key stored in .env

--- index.mjs
+++ index.mjs
@@ -1,28 +1,40 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 const PORT = 9000;
 import file from "./data.json" assert { type: "json" };
 
-app.set('view engine','ejs')
+// Set up
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static("public"));
 
-//GET request response giving the entire JSON array for all recipes
-app.get("/", (req, res) => {
-  res.send(file);
-});
+//Middleware to check api_key in request parameters against .env key
+const authChecker = (req, res, next) => {
+  if (process.env.APIKEY === req.params.api_key) {
+    next();
+  } else {
+    res.send("please provide your valid API key as a parameter");
+  }
+};
 
 //GET request response giving available endpoints
 app.get("/info", (req, res) => {
-  res.render('info', {data: file});
+  res.render("info", { data: file });
+});
+
+//GET request response giving the entire JSON array for all recipes
+app.get("/:api_key", authChecker, (req, res) => {
+  res.send(file);
 });
 
 //GET request responses giving the JSON object for each recipe
-for (let i = 1; i <= file.length; i++){
-app.get(`/recipe${i}`, (req,res)=> {
-    res.send(file[i-1])
-}) 
+for (let i = 0; i < file.length; i++) {
+  app.get(`/recipe${i + 1}/:api_key`, (req, res) => {
+    res.send(file[i]);
+  });
 }
 
-
+//serving app on localhost
 app.listen(PORT, () => console.log("Server live on http://localhost:" + PORT));
